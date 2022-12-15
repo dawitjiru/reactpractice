b909f9801a2eeeefe8373d19a08d2a06b87e787b
Dawit Jiru <dawit.jiru@gmail.com>
2022-12-15 14:44:20 -0600

    first project upload commit

---
+++ src/components/HelloReact.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function HelloReact() {
+  return (
+    <div>
+      <p>This is made using a key work Function</p>
+      <div>Replacing the Arrow Function Method</div>
+    </div>
+  );
+}
+
+// const HelloReact = () => {
+//   return (
+//     <div>
+//       <p>Hello World!</p>
+//       <div>Hello React</div>
+//     </div>
+//   );
+// };
+
+export default HelloReact;
